5eda8c2b715039192f90cca8880e35106b21b64b
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-10-03 01:21:20 +0430

    Refactor: make Navbar responsive

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,86 +1,140 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { NavLink } from "react-router-dom";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { twMerge } from "tailwind-merge";
 
 const navItems = [
-  { id: 1, path: "/", value: "Home" },
-  { id: 2, path: "/posts", value: "Posts" },
-  { id: 3, path: "/about", value: "About" },
-  { id: 4, path: "/contact", value: "Contact Us" },
+  { id: 1, path: "/", name: "Home" },
+  { id: 2, path: "/posts", name: "Posts" },
+  { id: 3, path: "/about", name: "About" },
+  { id: 4, path: "/contact", name: "Contact Us" },
+];
+
+const menuItems = [
+  { id: 1, name: "Your Profile" },
+  { id: 2, name: "Settings" },
+  { id: 3, name: "Sign out" },
 ];
 
 const Navbar = () => {
   return (
-    <div className="relative flex h-16 justify-between px-4">
-      <div className="flex flex-1 items-center justify-start">
-        <div className="flex flex-shrink-0 items-center">
-          <img className="h-8 w-auto" src={Logo} alt="Your Company" />
-        </div>
-        <div className="ml-6 flex space-x-8">
-          {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-          {navItems.map((item) => (
-            <NavLink
-              key={item.id}
-              to={item.path}
-              style={({ isActive }) =>
-                isActive ? { borderColor: "#6366f1" } : {}
-              }
-              className={
-                "inline-flex items-center border-b-2 hover:border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
-              }
-            >
-              {item.value}
-            </NavLink>
-          ))}
-        </div>
-      </div>
-
-      <div className="inset-y-0 right-0 flex items-center pr-2 inset-auto ml-6 sm:pr-0">
-        {/* Profile dropdown */}
-        <Menu as="div" className="relative ml-3">
-          <div>
-            <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">Open user menu</span>
-              <img
-                alt=""
-                src="https://lh3.googleusercontent.com/a/ACg8ocKHDO-ahUnPpzUb5rxTv3nzmEjIR1XlGncRbjqohXskGXuJzJmJ=s96-c-rg-br100"
-                className="h-8 w-8 rounded-full"
-              />
-            </MenuButton>
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link to={"/"}>
+                    <img className="h-8 w-auto" src={Logo} alt="logo" />
+                  </Link>
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  {navItems.map((item) => (
+                    <NavLink
+                      key={item.id}
+                      to={item.path}
+                      style={({ isActive }) =>
+                        isActive ? { borderColor: "#6366f1" } : {}
+                      }
+                      className="inline-flex items-center border-b-2 hover:border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                    >
+                      {item.name}
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://lh3.googleusercontent.com/a/ACg8ocKHDO-ahUnPpzUb5rxTv3nzmEjIR1XlGncRbjqohXskGXuJzJmJ=s96-c-rg-br100"
+                        alt="img"
+                      />
+                    </MenuButton>
+                  </div>
+                  <Transition
+                    enter="transition ease-out duration-200"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      {menuItems.map((item) => (
+                        <MenuItem key={item.id}>
+                          {({ focus }) => (
+                            <a
+                              href="#"
+                              className={twMerge(
+                                focus ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              {item.name}
+                            </a>
+                          )}
+                        </MenuItem>
+                      ))}
+                    </MenuItems>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
           </div>
-          <MenuItems
-            transition
-            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-          >
-            <MenuItem>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-              >
-                Your Profile
-              </a>
-            </MenuItem>
-            <MenuItem>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-              >
-                Settings
-              </a>
-            </MenuItem>
-            <MenuItem>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 hover:text-red-500"
-              >
-                Sign out
-              </a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-      </div>
-    </div>
+
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 pb-4 pt-2">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+
+              {navItems.map((item) => (
+                <DisclosureButton
+                  key={item.id}
+                  as={NavLink}
+                  to={item.path}
+                  style={({ isActive }) =>
+                    isActive ? { borderColor: "#6366f1" } : {}
+                  }
+                  className="block border-l-4 hover:border-indigo-500 py-2 pl-3 pr-4 text-base font-medium "
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
